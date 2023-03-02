80fffd0c3062c11bff2df5306210b4d515cd36f7
zisilva <zimarlensilva@gmail.com>
2023-03-02 13:19:33 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
